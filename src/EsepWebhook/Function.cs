9b0738fba373122fc463e9b6ffeddbf065df18ec
Chase Wrenn <chasewrenn@ufl.edu>
2023-03-28 22:13:50 -0400

    updated function code

--- src/EsepWebhook/Function.cs
+++ src/EsepWebhook/Function.cs
@@ -1,4 +1,8 @@
+
+
+using System.Text;
 using Amazon.Lambda.Core;
+using Newtonsoft.Json;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -7,15 +11,28 @@ namespace EsepWebhook;
 
 public class Function
 {
-    
+
     /// <summary>
     /// A simple function that takes a string and does a ToUpper
     /// </summary>
     /// <param name="input"></param>
     /// <param name="context"></param>
     /// <returns></returns>
-    public string FunctionHandler(string input, ILambdaContext context)
+    public string FunctionHandler(object input, ILambdaContext context)
     {
-        return input.ToUpper();
+        dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
+
+        string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
+
+        var client = new HttpClient();
+        var webRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SLACK_URL"))
+        {
+            Content = new StringContent(payload, Encoding.UTF8, "application/json")
+        };
+
+        var response = client.Send(webRequest);
+        using var reader = new StreamReader(response.Content.ReadAsStream());
+
+        return reader.ReadToEnd();
     }
 }
